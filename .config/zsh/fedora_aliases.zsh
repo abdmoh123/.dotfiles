a602800c2f951407556bc87a9a163b312a2e7e2a
abdmoh123 <abdhawisa@gmail.com>
2024-04-19 00:35:38 +0100

    Hide filesize in ll alias

--- .config/zsh/fedora_aliases.zsh
+++ .config/zsh/fedora_aliases.zsh
@@ -1,5 +1,5 @@
 # ls aliases
 alias ls="eza --color=always --icons=always"
-alias ll="eza --long --no-permissions --no-time --no-user --color=always --icons=always"
+alias ll="eza --long --no-permissions --no-filesize --no-time --no-user --color=always --icons=always"
 alias tree="eza --tree --level=2 --color=always --icons=always"
 
