01e04477925907efdb14e88c06201b0af8f4f2c0
abdmoh123 <abdhawisa@gmail.com>
2024-04-19 17:12:38 +0100

    Aliase cd to zoxide

--- .config/zsh/fedora_aliases.zsh
+++ .config/zsh/fedora_aliases.zsh
@@ -3,3 +3,6 @@ alias ls="eza --color=always --icons=always"
 alias ll="eza --long --no-permissions --no-filesize --no-time --no-user --color=always --icons=always"
 alias tree="eza --tree --level=2 --color=always --icons=always"
 
+# zoxide alias
+alias cd="z"
+
