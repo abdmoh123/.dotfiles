1ae78d38f5dfea28c230f25b476809e4ed74f432
abdmoh123 <abdhawisa@gmail.com>
2024-04-23 22:07:27 +0100

    Configure gruvbox-material to be transparent

--- .config/nvim/lua/custom/themes.lua
+++ .config/nvim/lua/custom/themes.lua
@@ -1,5 +1,14 @@
 -- File: lua/custom/plugins/themes.lua
 
+-- gruvbox material settings
+vim.g.gruvbox_material_background = 'medium'
+vim.g.gruvbox_material_foreground = 'mix'
+vim.g.gruvbox_material_transparent_background = 1
+vim.g.gruvbox_material_statusline_style = 'mix'
+vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_better_performance = 1
+
 return {
   {
     -- gruvbox dark and light theme
