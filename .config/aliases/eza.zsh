7f88e2257bb57a318135d37378a312dd99955f47
abdmoh123 <abdhawisa@gmail.com>
2024-04-28 00:39:18 +0100

    Improve and add more eza aliases

--- .config/aliases/eza.zsh
+++ .config/aliases/eza.zsh
@@ -1,5 +1,11 @@
 # eza aliases
 alias ls="eza --color=always --icons=always"
-alias ll="eza --long --no-permissions --no-filesize --no-time --no-user --color=always --icons=always"
-alias tree="eza --tree --level=2 --color=always --icons=always"
+alias la="eza -a --color=always --icons=always"
+alias ll="eza -l --no-permissions --no-filesize --no-time --no-user --color=always --icons=always"
+alias lla="eza -la --no-permissions --no-filesize --no-time --no-user --color=always --icons=always"
+alias lah="eza -lah --color=always --icons=always"
 
+alias tree="eza --tree --level=2 --color=always --icons=always"
+alias treea="eza --tree -a --level=2 --color=always --icons=always"
+alias treel="eza --tree --color=always --icons=always"
+alias treela="eza --tree -a --color=always --icons=always"
