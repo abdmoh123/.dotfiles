bbcdd4379e7af69088ffbaff1a12e28f5eebcc10
abdmoh123 <abdhawisa@gmail.com>
2024-05-12 02:18:48 +0100

    Make status line global, add file size information, and make it rounder

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,12 +5,24 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      theme = 'gruvbox-material',
       icons_enabled = true,
       component_separators = { left = '│', right = '│' },
-      section_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
       disabled_filetypes = {
         statusline = { 'neo-tree' },
+      },
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = {
+        { 'mode', separator = { left = '' }, right_padding = 2 },
+      },
+      lualine_c = {
+        { 'filename' },
+        { 'filesize' },
+      },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 2 },
       },
     },
   },
