3b7cb5caec6c6df3588aa9270265086ea0af3f06
abdmoh123 <abdhawisa@gmail.com>
2024-05-12 20:54:55 +0100

    Use zoxide for fzf alt-c (windows)

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -22,7 +22,7 @@ $env:FZF_DEFAULT_OPTS=$env:FZF_DEFAULT_THEME
 # Enable keybinds
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 # Override cd command (use zoxide instead)
-$commandOverride = [ScriptBlock]{ param($Location) $Location }
+$commandOverride = [ScriptBlock]{ param($Location) z $Location }
 Set-PsFzfOption -AltCCommand $commandOverride
 
 # Import aliases
