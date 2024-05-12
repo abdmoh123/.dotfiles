40e7d5a12de8c12236f9cc975c944d431f7842e4
abdmoh123 <abdhawisa@gmail.com>
2024-05-12 22:18:24 +0100

    Add more keybinds for bufferline

--- .config/nvim/lua/keybindings/bufferline.lua
+++ .config/nvim/lua/keybindings/bufferline.lua
@@ -2,3 +2,10 @@
 
 vim.keymap.set('n', '<leader>bn', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Switch to next buffer' })
 vim.keymap.set('n', '<leader>bp', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Switch to previous buffer' })
+vim.keymap.set('n', '<leader>b>', '<Cmd>BufferLineMoveNext<CR>', { desc = 'Move current buffer right' })
+vim.keymap.set('n', '<leader>b<', '<Cmd>BufferLineMovePrev<CR>', { desc = 'Move current buffer left' })
+vim.keymap.set('n', '<leader>bdr', '<Cmd>BufferLineCloseRight<CR>', { desc = 'Close all visible buffers to the right' })
+vim.keymap.set('n', '<leader>bdl', '<Cmd>BufferLineCloseLeft<CR>', { desc = 'Close all visible buffers to the left' })
+vim.keymap.set('n', '<leader>bdd', '<Cmd>BufferLineCloseOthers<CR>', { desc = 'Close all other visible buffers' })
+vim.keymap.set('n', '<leader>bP', '<Cmd>BufferLinePick<CR>', { desc = 'Pick a buffer' })
+vim.keymap.set('n', '<leader>bD', '<Cmd>BufferLinePickClose<CR>', { desc = 'Pick a buffer to close' })
