6e0c98d85596ccda309fabe9b71881beb431db11
abdmoh123 <abdhawisa@gmail.com>
2024-05-15 00:29:46 +0100

    Add description to neotree keybind

--- .config/nvim/lua/keybindings/neo-tree.lua
+++ .config/nvim/lua/keybindings/neo-tree.lua
@@ -1,4 +1,4 @@
 -- File: lua/keybindings/neo-tree.lua
 
 -- Set Neotree toggle keymap
-vim.keymap.set('n', '<leader><Tab>', '<Cmd>Neotree toggle<CR>')
+vim.keymap.set('n', '<leader><Tab>', '<Cmd>Neotree reveal<CR>', { desc = 'Reveal Explorer' })
