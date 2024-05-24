479314b596f544d367aff8699029d1a3db0705bc
abdmoh123 <abdhawisa@gmail.com>
2024-05-24 21:39:10 +0100

    Improve buffer pick keybind

--- .config/nvim/lua/keybindings/bufferline.lua
+++ .config/nvim/lua/keybindings/bufferline.lua
@@ -7,5 +7,5 @@ vim.keymap.set('n', '<leader>b<', '<Cmd>BufferLineMovePrev<CR>', { desc = 'Move 
 vim.keymap.set('n', '<leader>bdr', '<Cmd>BufferLineCloseRight<CR>', { desc = '[D]elete all visible [B]uffers to the [R]ight' })
 vim.keymap.set('n', '<leader>bdl', '<Cmd>BufferLineCloseLeft<CR>', { desc = '[D]elete all visible [B]uffers to the [L]eft' })
 vim.keymap.set('n', '<leader>bdd', '<Cmd>BufferLineCloseOthers<CR>', { desc = '[D]elete all other visible [B]uffers' })
-vim.keymap.set('n', '<leader>bP', '<Cmd>BufferLinePick<CR>', { desc = '[P]ick a [B]uffer' })
+vim.keymap.set('n', '<leader>bp', '<Cmd>BufferLinePick<CR>', { desc = '[P]ick a [B]uffer' })
 vim.keymap.set('n', '<leader>bD', '<Cmd>BufferLinePickClose<CR>', { desc = 'Pick a [B]uffer to [D]elete' })
