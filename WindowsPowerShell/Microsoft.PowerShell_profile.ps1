4a1757a09bf5fc28deb8e54caa2c7b57d5bf4b0d
abdmoh123 <abdhawisa@gmail.com>
2024-06-07 22:12:17 +0100

    Add chocolatey autocomplete

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -7,3 +7,13 @@ Import-Module posh-git
 # Enable zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
