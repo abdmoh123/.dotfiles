e5e215ffa33d2279771fc169a6a1258cd5cabe11
abdmoh123 <abdhawisa@gmail.com>
2024-06-07 22:44:40 +0100

    Add folding preview functionality

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -6,6 +6,25 @@ return {
       provider_selector = function(bufnr, filetype, buftype)
         return { 'treesitter', 'indent' }
       end,
+      preview = {
+        win_config = {
+          winhighlight = 'Normal:Folded',
+          winblend = 0,
+        },
+      },
+      mappings = {
+        scrollU = '<C-u>',
+        scrollD = '<C-d>',
+      },
     }
+
+    -- folding keybinds using ufo
+    vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
+    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
+
+    -- preview fold
+    vim.keymap.set('n', 'zp', function()
+      require('ufo').peekFoldedLinesUnderCursor()
+    end, { desc = 'Preview fold' })
   end,
 }
