0ae1d59b514e907a458bd987133c46b107bab5c8
abdmoh123 <abdhawisa@gmail.com>
2024-06-10 22:56:34 +0100

    Make window swapping harder to accidentally do

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -7,9 +7,9 @@ return {
     vim.keymap.set('n', '<A-J>', require('smart-splits').resize_down, { desc = 'Reize window down' })
     vim.keymap.set('n', '<A-K>', require('smart-splits').resize_up, { desc = 'Reize window up' })
     -- swapping buffers between windows
-    vim.keymap.set('n', '<C-h>', require('smart-splits').swap_buf_left, { desc = 'Swap with window to left' })
-    vim.keymap.set('n', '<C-l>', require('smart-splits').swap_buf_right, { desc = 'Swap with window to right' })
-    vim.keymap.set('n', '<C-j>', require('smart-splits').swap_buf_down, { desc = 'Swap with window below' })
-    vim.keymap.set('n', '<C-k>', require('smart-splits').swap_buf_up, { desc = 'Swap with window above' })
+    vim.keymap.set('n', '<C-W><A-h>', require('smart-splits').swap_buf_left, { desc = 'Swap with window to left' })
+    vim.keymap.set('n', '<C-W><A-l>', require('smart-splits').swap_buf_right, { desc = 'Swap with window to right' })
+    vim.keymap.set('n', '<C-W><A-j>', require('smart-splits').swap_buf_down, { desc = 'Swap with window below' })
+    vim.keymap.set('n', '<C-W><A-k>', require('smart-splits').swap_buf_up, { desc = 'Swap with window above' })
   end,
 }
