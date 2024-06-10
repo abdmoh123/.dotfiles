e943fa4932f2d0339c69db1fb3cf2a642d288df2
abdmoh123 <abdhawisa@gmail.com>
2024-06-10 22:05:51 +0100

    Modify gruvbox-material config
    - Switch to hard background
    - Change cursor colour to orange
    - Enable coloured diagnostics hints

--- .config/nvim/lua/themes/configs/gruvbox-material.lua
+++ .config/nvim/lua/themes/configs/gruvbox-material.lua
@@ -1,9 +1,13 @@
 -- File: lua/custom/plugins/themes/gruvbox-material.lua
 
-vim.g.gruvbox_material_background = 'medium'
+vim.g.gruvbox_material_background = 'hard'
 vim.g.gruvbox_material_foreground = 'mix'
 vim.g.gruvbox_material_transparent_background = 2
 vim.g.gruvbox_material_statusline_style = 'mix'
+vim.g.gruvbox_material_cursor = 'orange'
+
+vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+
 vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_better_performance = 1
