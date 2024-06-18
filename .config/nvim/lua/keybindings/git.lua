bdcd4258a4644886c2138f5f0e4ef3a3fcdef5ef
abdmoh123 <abdhawisa@gmail.com>
2024-06-19 00:32:13 +0100

    Add git signs reset key bindings

--- .config/nvim/lua/keybindings/git.lua
+++ .config/nvim/lua/keybindings/git.lua
@@ -3,8 +3,12 @@
 -- [[ Gitsigns keybindings ]]
 vim.keymap.set('n', '<leader>gK', '<cmd>Gitsigns preview_hunk<cr>', { desc = 'Preview [G]it hunk' })
 vim.keymap.set('n', '<leader>gd', '<cmd>Gitsigns diffthis<cr>', { desc = '[G]it [D]iff current buffer' })
+
 vim.keymap.set('n', '<leader>ga', '<cmd>Gitsigns stage_hunk<cr>', { desc = '[G]it [A]dd/stage current hunk' })
 vim.keymap.set('n', '<leader>gA', '<cmd>Gitsigns stage_buffer<cr>', { desc = '[G]it [A]dd/stage current buffer' })
+
+vim.keymap.set('n', '<leader>gr', '<cmd>Gitsigns reset_hunk<cr>', { desc = '[G]it [R]eset current hunk' })
+vim.keymap.set('n', '<leader>gR', '<cmd>Gitsigns reset_buffer<cr>', { desc = '[G]it [R]eset current buffer' })
 -- git hunk navigation
 vim.keymap.set('n', ']h', '<cmd>Gitsigns next_hunk<cr>', { desc = 'Next Git [H]unk' })
 vim.keymap.set('n', '[h', '<cmd>Gitsigns prev_hunk<cr>', { desc = 'Previous Git [H]unk' })
