4060eb099794092dd761752af49bf6b1122cede5
abdmoh123 <abdhawisa@gmail.com>
2024-06-20 00:23:36 +0100

    Improve dashboard appearance - Add help option - Change icon choice for options - Add version info in footer

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,10 +1,9 @@
 return {
   'goolord/alpha-nvim',
   config = function()
-    -- require('alpha').setup(require('alpha.themes.dashboard').config)
-
     local alpha = require 'alpha'
     local dashboard = require 'alpha.themes.dashboard'
+
     dashboard.section.header.val = {
       [[                               __                ]],
       [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
@@ -13,19 +12,22 @@ return {
       [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
       [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
     }
+
+    -- display things to do
     dashboard.section.buttons.val = {
-      dashboard.button('e', '  New file', '<cmd>ene <CR>'),
-      dashboard.button('SPC s f', '󰈞  Find file'),
-      dashboard.button('SPC s .', '󰊄  Recently opened files'),
-      dashboard.button('SPC s F', '  Frecency/MRU'),
-      dashboard.button('SPC s g', '󰈬  Find word (ripgrep)'),
-      dashboard.button('SPC h', '  View harpoon bookmarks'),
-      dashboard.button('q', '󰅚  Quit Neovim', ':qa<CR>'),
+      dashboard.button('e', '󰈔  New file', '<cmd>ene <CR>'),
+      dashboard.button('SPC s f', '󰱼  Find file'),
+      dashboard.button('SPC s .', '󰋚  Recently opened files'),
+      dashboard.button('SPC s F', '󰑖  Frecency/MRU'),
+      dashboard.button('SPC s g', '󰦨  Find word (ripgrep)'),
+      dashboard.button('SPC h', '󰃀  View harpoon bookmarks'),
+      dashboard.button('?', '󰋗  Neovim help', ':h nvim<CR>'),
+      dashboard.button('q', '󰅙  Quit Neovim', ':qa<CR>'),
     }
-    local handle = io.popen 'fortune'
-    local fortune = handle:read '*a'
-    handle:close()
-    dashboard.section.footer.val = fortune
+
+    -- display neovim version at the bottom
+    local version_str = 'v' .. vim.version().major .. '.' .. vim.version().minor .. '.' .. vim.version().patch
+    dashboard.section.footer.val = version_str
 
     dashboard.config.opts.noautocmd = true
 
