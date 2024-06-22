c910e81353f230b158f92ffe982ae501225d8f6a
abdmoh123 <abdhawisa@gmail.com>
2024-06-22 02:41:39 +0100

    Improve eza binds (add lsa and fix issues with replacing ls alias)

--- PowerShell/Aliases/eza.ps1
+++ PowerShell/Aliases/eza.ps1
@@ -1,12 +1,23 @@
 # eza aliases
-Remove-Alias -Name ls
-function ls { eza --color=always --icons=always --group-directories-first @args }
-function la { eza -a --color=always --icons=always --group-directories-first @args }
-function ll { eza -l --no-permissions --no-filesize --no-time --no-user --color=always --icons=always --group-directories-first @args }
-function lla { eza -la --no-permissions --no-filesize --no-time --no-user --color=always --icons=always --group-directories-first @args }
-function lah { eza -lah --color=always --icons=always --group-directories-first @args }
+Set-Alias -Name ls -Value new_ls
+Set-Alias -Name la -Value new_la
+Set-Alias -Name lsa -Value new_la
+Set-Alias -Name ll -Value new_ll
+Set-Alias -Name lla -Value new_lla
+Set-Alias -Name lah -Value new_lah
 
-function tree { eza --tree --level=2 --color=always --icons=always --group-directories-first @args }
-function treea { eza --tree -a --level=2 --color=always --icons=always --group-directories-first @args }
-function treel { eza --tree --color=always --icons=always --group-directories-first @args }
-function treela { eza --tree -a --color=always --icons=always --group-directories-first @args }
+function new_ls { eza --color=always --icons=always --group-directories-first @args }
+function new_la { eza -a --color=always --icons=always --group-directories-first @args }
+function new_ll { eza -l --no-permissions --no-filesize --no-time --no-user --color=always --icons=always --group-directories-first @args }
+function new_lla { eza -la --no-permissions --no-filesize --no-time --no-user --color=always --icons=always --group-directories-first @args }
+function new_lah { eza -lah --color=always --icons=always --group-directories-first @args }
+
+Set-Alias -Name tree -Value new_tree
+Set-Alias -Name treea -Value new_treea
+Set-Alias -Name treel -Value new_treel
+Set-Alias -Name treela -Value new_treela
+
+function new_tree { eza --tree --level=2 --color=always --icons=always --group-directories-first @args }
+function new_treea { eza --tree -a --level=2 --color=always --icons=always --group-directories-first @args }
+function new_treel { eza --tree --color=always --icons=always --group-directories-first @args }
+function new_treela { eza --tree -a --color=always --icons=always --group-directories-first @args }
