e1cdfacd7ac267b94d63d3d6fafaffd1b16df78f
abdmoh123 <abdhawisa@gmail.com>
2024-10-04 00:52:56 +0100

    Switch to default git signs

--- .config/nvim/lua/kickstart/plugins/gitsigns.lua
+++ .config/nvim/lua/kickstart/plugins/gitsigns.lua
@@ -10,13 +10,4 @@
 return {
   'lewis6991/gitsigns.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
 }
