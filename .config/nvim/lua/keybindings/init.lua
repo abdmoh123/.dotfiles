3f96419950dfec4e1e4ae000c1b23ede2790f753
abdmoh123 <abdhawisa@gmail.com>
2024-10-08 01:28:05 +0100

    Remove harpoon keybindings include

--- .config/nvim/lua/keybindings/init.lua
+++ .config/nvim/lua/keybindings/init.lua
@@ -5,7 +5,6 @@
 require 'keybindings.bufferline'
 require 'keybindings.neo-tree'
 require 'keybindings.diagnostics'
-require 'keybindings.harpoon'
 require 'keybindings.terminal'
 require 'keybindings.movement'
 require 'keybindings.git'
