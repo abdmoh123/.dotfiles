1120e888e963f5e82882342c912e0a339dba48bf
abdmoh123 <abdhawisa@gmail.com>
2024-10-15 00:33:47 +0100

    Configure noice.nvim to get rid of warnings in checkhealth noice

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -2,7 +2,16 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
-    -- add any options here
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {},
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
