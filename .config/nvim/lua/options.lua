111e1eb5545dd5ad66afe8f0b517560a36bf114a
abdmoh123 <abdhawisa@gmail.com>
2024-12-08 22:13:31 +0000

    Set default shell to cmd but set toggleterm to pwsh
    - Should improve performance of neovim without changing
      the shell of the integrated terminal (toggle-term)

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,17 +4,6 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Set default shell to powershell if possible (windows only)
-if vim.fn.has 'win32' == 1 or vim.fn.has 'win64' == 1 then
-  vim.opt.shell = vim.fn.executable 'pwsh' and 'pwsh' or 'powershell'
-  vim.opt.shellcmdflag =
-    '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-  vim.opt.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
-  vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-  vim.opt.shellquote = ''
-  vim.opt.shellxquote = ''
-end
-
 -- [[ General UI settings ]]
 vim.opt.background = 'dark'
 
--- .config/nvim/lua/plugins/toggle-term.lua
+++ .config/nvim/lua/plugins/toggle-term.lua
@@ -3,4 +3,15 @@ return {
   event = 'VeryLazy',
   version = '*',
   config = true,
+  opts = {
+    shell = function()
+      if vim.fn.has 'win32' == 1 or vim.fn.has 'win64' == 1 then
+        -- use pwsh if windows
+        return 'pwsh -NoLogo'
+      else
+        -- use bash if linux
+        return '/bin/bash'
+      end
+    end,
+  },
 }
