dfe457fa8c0a10bcea62427d20854375b54cbd39
abdmoh123 <abdhawisa@gmail.com>
2024-12-15 16:36:37 +0000

    Add snacks.nvim bigfile module

---
+++ .config/nvim/lua/plugins/snacks-nvim.lua
@@ -0,0 +1,26 @@
+return {
+  'folke/snacks.nvim',
+  ---@type snacks.Config
+  opts = {
+    bigfile = {
+      notify = true,
+      size = 50.0 * 1024 * 1024, -- 20MB
+      ---@param ctx {buf: number, ft: string}
+      setup = function(ctx)
+        vim.cmd [[NoMatchParen]]
+        Snacks.util.wo(0, { foldmethod = 'manual', spell = false, statuscolumn = '', conceallevel = 0 })
+        vim.b.minianimate_disable = true
+        vim.schedule(function()
+          vim.bo[ctx.buf].syntax = ctx.ft
+        end)
+        vim.cmd 'UfoDetach'
+        vim.cmd 'Gitsigns detach'
+        vim.cmd 'set norelativenumber'
+        vim.cmd 'syntax off'
+        vim.cmd 'TSBufDisable highlight'
+        vim.cmd 'TSBufDisable incremental_selection'
+        vim.cmd 'TSBufDisable indent'
+      end,
+    },
+  },
+}
