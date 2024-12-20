763f4dcad75d7c00e238337728c58d2b00cd8d8e
abdmoh123 <abdhawisa@gmail.com>
2024-12-20 21:07:29 +0000

    Change oil.nvim window to be a floating one

--- .config/nvim/lua/keybindings/file-explorer.lua
+++ .config/nvim/lua/keybindings/file-explorer.lua
@@ -3,4 +3,4 @@
 -- Set Neotree toggle keymap
 vim.keymap.set('n', '<leader><Tab>', ':Neotree float<CR>', { desc = 'Reveal Explorer', silent = true })
 -- Set oil.nvim keymap
-vim.keymap.set('n', '<leader>-', ':Oil<CR>', { desc = 'Open directory in oil.nvim' })
+vim.keymap.set('n', '<leader>-', ":lua require('oil').open_float()<CR>", { desc = 'Open directory in oil.nvim' })
