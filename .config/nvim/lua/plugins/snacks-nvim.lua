fbef6f2418b16da62fdec9b4218881647ee8d145
abdmoh123 <abdhawisa@gmail.com>
2025-01-04 02:37:15 +0000

    Enable more snacks.nvim features + remove lazygit.nvim plugin
    - Increase loading priority of snacks.nvim
    - Use built-in lazygit feature instead of lazygit.nvim plugin
        - Temporarily disable use of nvim-remote (editing in
          current instance of neovim) due to freezing
            - Unfortunately will cause nested windows when used
    - Add keybinds for jumping across symbol references
    - Add keybind for getting commit details on git blame line
    - Add various toggles for neovim settings
        - Toggle line numbers
        - Toggle relative line numbers
        - Toggle wrap
        - Toggle diagnostics
        - Toggle treesitter
        - Toggle inlay hints (e.g. `func(num: 1, num: 2)`)
        - Toggle light/dark mode
        - Toggle scope based dimming (only current scope is highlighted)

--- .config/nvim/lua/plugins/snacks-nvim.lua
+++ .config/nvim/lua/plugins/snacks-nvim.lua
@@ -1,5 +1,7 @@
 return {
   'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
   ---@type snacks.Config
   opts = {
     bigfile = {
@@ -23,9 +25,90 @@ return {
       end,
     },
     -- sets lazygit colorscheme to match neovim's
-    lazygit = { enabled = true },
-    -- speeds up file loading (e.g. nvim foo.txt)
-    quickfile = { enabled = true },
-    words = { enabled = true },
+    ---@class snacks.lazygit.Config: snacks.terminal.Opts
+    ---@field args? string[]
+    ---@field theme? snacks.lazygit.Theme
+    lazygit = {
+      configure = true,
+      config = {
+        -- WARNING: Will result in nested windows (workaround until nvim-remote stops freezing)
+        os = { editPreset = 'nvim' },
+        gui = {
+          -- set to an empty string "" to disable icons
+          nerdFontsVersion = '3',
+        },
+      },
+    },
+    quickfile = { enabled = true }, -- speeds up inputted file loading (e.g. nvim foo.txt)
+    words = { enabled = true }, -- allow jumping between references of a symbol (e.g. variable)
   },
+  keys = {
+    {
+      '<leader>gl',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Open Lazygit',
+    },
+    {
+      '<leader>gB',
+      function()
+        Snacks.git.blame_line()
+      end,
+      desc = 'Get git blame details',
+    },
+    {
+      '<leader>rf',
+      function()
+        Snacks.rename.rename_file()
+      end,
+      desc = '[R]ename [F]ile',
+    },
+    {
+      ']r',
+      function()
+        Snacks.words.jump(vim.v.count1)
+      end,
+      desc = 'Next Reference',
+      mode = { 'n', 't' },
+    },
+    {
+      '[r',
+      function()
+        Snacks.words.jump(-vim.v.count1)
+      end,
+      desc = 'Previous Reference',
+      mode = { 'n', 't' },
+    },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+        -- [[ Create some toggle mappings ]]
+
+        Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>tw'
+        Snacks.toggle.line_number():map '<leader>tl'
+        Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>tL'
+
+        Snacks.toggle.diagnostics():map '<leader>td'
+        Snacks.toggle.treesitter():map '<leader>tT' -- treesitter highlighting
+        Snacks.toggle.inlay_hints():map '<leader>th' -- example 'func([str: ]"hello");' inlay_hint = in square brackets
+
+        -- toggle between dark and light background
+        Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>tb'
+        -- toggle scope dimming feature (e.g. only current function is highlighted)
+        Snacks.toggle.dim():map '<leader>tD'
+      end,
+    })
+  end,
 }
