c4674effc5f43315f7091f60c9ad7d8abcb00872
abdmoh123 <abdhawisa@gmail.com>
2025-01-04 03:05:32 +0000

    Actually fix neotree now (options are now respected properly)

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -10,7 +10,17 @@ return {
   },
   cmd = { 'Neotree' },
   opts = function(_, opts)
-    require('neo-tree').setup {
+    local function on_move(data)
+      Snacks.rename.on_rename_file(data.source, data.destination)
+    end
+    local events = require 'neo-tree.events'
+    opts.event_handlers = opts.event_handlers or {}
+    vim.list_extend(opts.event_handlers, {
+      { event = events.FILE_MOVED, handler = on_move },
+      { event = events.FILE_RENAMED, handler = on_move },
+    })
+
+    return {
       source_selector = {
         winbar = false,
         statusline = false,
@@ -32,16 +42,6 @@ return {
         ['fxml'] = { 'java' },
       },
     }
-
-    local function on_move(data)
-      Snacks.rename.on_rename_file(data.source, data.destination)
-    end
-    local events = require 'neo-tree.events'
-    opts.event_handlers = opts.event_handlers or {}
-    vim.list_extend(opts.event_handlers, {
-      { event = events.FILE_MOVED, handler = on_move },
-      { event = events.FILE_RENAMED, handler = on_move },
-    })
   end,
   keys = {
     { '<leader><Tab>', '<CMD>Neotree filesystem reveal left<CR>', desc = 'Reveal Explorer', silent = true },
