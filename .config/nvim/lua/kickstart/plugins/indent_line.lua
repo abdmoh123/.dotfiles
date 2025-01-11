8d6a8336f4fcbaf0331185b393ef547fa3044e21
abdmoh123 <abdhawisa@gmail.com>
2025-01-11 15:42:08 +0000

    Switch rainbow delimiter/indents highlights to RainbowDelimiter
    - e.g. rainbowcol1 -> RainbowDelimiterRed

--- .config/nvim/lua/kickstart/plugins/indent_line.lua
+++ .config/nvim/lua/kickstart/plugins/indent_line.lua
@@ -7,13 +7,13 @@ return {
   main = 'ibl',
   config = function()
     local rainbow_highlight_colours = {
-      'rainbowcol1',
-      'rainbowcol2',
-      'rainbowcol3',
-      'rainbowcol4',
-      'rainbowcol5',
-      'rainbowcol6',
-      'rainbowcol7',
+      'RainbowDelimiterRed',
+      'RainbowDelimiterOrange',
+      'RainbowDelimiterYellow',
+      'RainbowDelimiterGreen',
+      'RainbowDelimiterCyan',
+      'RainbowDelimiterBlue',
+      'RainbowDelimiterViolet',
     }
 
     require('ibl').setup {
