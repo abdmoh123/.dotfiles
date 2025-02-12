4cacb2747ebe742b8f4fd28b6646359348b7c91f
abdmoh123 <abdhawisa@gmail.com>
2025-02-12 23:48:36 +0000

    tweak(mini): mini files window border to be round

--- .config/nvim/lua/kickstart/plugins/mini.lua
+++ .config/nvim/lua/kickstart/plugins/mini.lua
@@ -59,5 +59,17 @@ return {
         end, { desc = 'Reset to project cwd' })
       end,
     })
+
+    -- make window border round
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesWindowOpen',
+      callback = function(args)
+        local win_id = args.data.win_id
+
+        local config = vim.api.nvim_win_get_config(win_id)
+        config.border = 'rounded'
+        vim.api.nvim_win_set_config(win_id, config)
+      end,
+    })
   end,
 }
