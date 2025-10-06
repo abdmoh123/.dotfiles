233faf6d62f01f85d2c6d77982f03dbd4757d25d
abdmoh123 <abdhawisa@gmail.com>
2025-10-07 00:03:33 +0100

    refactor(nvim): move kickstart plugins to plugins folder

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -10,38 +10,13 @@
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- Used for configuring Lua LSP for neovim
-  require 'kickstart.plugins.lazydev',
-  -- Make commenting out code easier
-  require 'kickstart.plugins.comment-nvim',
-  -- Adds git related signs to the gutter, as well as utilities for managing changes
-  require 'kickstart.plugins.gitsigns',
-  -- Useful plugin to show you pending keybinds.
-  require 'kickstart.plugins.which-key',
-  -- Fuzzy Finder (files, lsp, etc)
-  require 'kickstart.plugins.telescope',
-  -- LSP Configuration & Plugins
-  require 'kickstart.plugins.lspconfig',
-  -- Autoformat
-  require 'kickstart.plugins.conform',
-  -- Highlight todo, notes, etc in comments
-  require 'kickstart.plugins.todo-comments',
-  -- Collection of various small independent plugins/modules
-  require 'kickstart.plugins.mini',
-  -- Highlight, edit, and navigate code
-  require 'kickstart.plugins.treesitter',
-
-  require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
-
   -- Import all custom themes (e.g. gruvbox-material)
   require 'themes',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
-  -- Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  -- Uncomment the following line and add your plugins to `lua/plugins/*.lua` to get going.
   -- For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- import all lazy.nvim plugins (lua/plugins directory)
   { import = 'plugins' },
