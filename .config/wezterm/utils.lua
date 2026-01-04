a925d133f08aeec180c72717e5901a3f5bd88850
abdmoh123 <abdhawisa@gmail.com>
2026-01-04 00:43:05 +0000

    refactor(wezterm): cleanup shell and OS related stuff
    Also added annotations to the functions in utils.lua for LSP
    autocomplete and better documentation.

--- .config/wezterm/utils.lua
+++ .config/wezterm/utils.lua
@@ -1,35 +1,126 @@
 local wezterm = require("wezterm")
 
+-- forward declarations for private functions
+local shell_exists_win, shell_exists_linux, generic_shell_finder
+
+---@class Utils
 local M = {}
 
-function M.shell_exists(shell)
-	local shells_handle = io.popen("ls /etc/shells")
-	if shells_handle == nil then
-		return false
+---@enum OSTypes
+M.OSTypes = {
+	Undefined = "Undefined",
+	Linux = "Linux",
+	MacOS = "macOS",
+	Windows = "Windows",
+}
+
+---@return string[]
+function M.get_default_shell()
+	if M.get_os() == M.OSTypes.Windows then
+		return { "cmd" }
+	elseif M.get_os() == M.OSTypes.Linux then
+		return { "bash", "--login" }
+	elseif M.get_os() == M.OSTypes.MacOS then
+		return { "zsh", "--login" }
+	else -- undefined
+		return {}
 	end
-
-	local shells = shells_handle:read("*a")
-	wezterm.log_info(shells)
-	shells_handle:close()
-
-	return string.find(shells, shell)
 end
 
-function M.shell_exists_win(shell)
-	local shells_handle = io.popen("where " .. shell)
-	if shells_handle == nil then
+---@return OSTypes
+function M.get_os()
+	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+		return M.OSTypes.Windows
+	elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+		return M.OSTypes.Linux
+	else
+		return M.OSTypes.Undefined
+	end
+end
+
+---@param filename string
+---@return boolean
+function M.file_exists(filename)
+	local f = io.open(filename, "r")
+	if f == nil then
 		return false
+	end
+	f:close()
+	return true
+end
+
+---@param filename string
+---@return string[]?
+function M.load_shell_command(filename)
+	local f = io.open(filename, "r")
+	if f == nil then
+		return nil
+	end
+	local shell_cmd_str = f:read("*a")
+	f:close()
+
+	local shell_cmd = {}
+	for str in shell_cmd_str:gmatch("[%a%p]+") do
+		table.insert(shell_cmd, str)
+	end
+
+	if next(shell_cmd) == nil then
+		return nil
+	end
+	return shell_cmd
+end
+
+---@param shell string
+---@param os_type OSTypes
+---@return boolean
+function M.shell_exists(shell, os_type)
+	if os_type == M.OSTypes.Windows then
+		return shell_exists_win(shell)
+	elseif os_type == M.OSTypes.Linux then
+		return shell_exists_linux(shell)
+	else
+		return false
+	end
+end
+
+---@param shell string
+---@return boolean
+function shell_exists_linux(shell)
+	local result = generic_shell_finder("cat /etc/shells")
+	if result ~= nil and string.find(result, shell) then
+		return true
+	else
+		return false
+	end
+end
+
+---@param shell string
+---@return boolean
+function shell_exists_win(shell)
+	local result = generic_shell_finder("where " .. shell)
+	if result == nil or result == "INFO: Could not find files for the given pattern(s)." then
+		return false
+	end
+
+	if string.find(result, shell) then
+		return true
+	else
+		return false
+	end
+end
+
+---@param shell_find_command string
+---@return string?
+function generic_shell_finder(shell_find_command)
+	local shells_handle = io.popen(shell_find_command)
+	if shells_handle == nil then
+		return nil
 	end
 
 	local shells = shells_handle:read("*a")
-	wezterm.log_info(shells)
 	shells_handle:close()
-
-	if shells == "INFO: Could not find files for the given pattern(s)." then
-		return false
-	end
-
-	return string.find(shells, shell)
+	-- wezterm.log_info(shells)
+	return shells
 end
 
 return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+---@type Utils
 local utils = require("utils")
 
 local config = wezterm.config_builder()
@@ -6,45 +7,24 @@ local config = wezterm.config_builder()
 config.max_fps = 120
 config.animation_fps = 60
 
-local nushell_cmd = { "nu", "--login" }
-local pwsh_cmd = { "pwsh", "-NoLogo" }
-local powershell_cmd = { "powershell", "-NoLogo" }
-local zsh_cmd = { "zsh", "--login" }
+-- [[ Shell related settings ]]
+local shell_cmd = utils.get_default_shell()
+config.default_prog = shell_cmd
+-- wezterm.log_info("Using shell (+ args): " .. table.concat(shell_cmd, " "))
 
--- [[ OS and shell related stuff ]]
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then -- windows specific
+-- [[ OS specific settings ]]
+local os_type = utils.get_os()
+if os_type == utils.OSTypes.Windows then
 	config.win32_system_backdrop = "Acrylic"
-
-	if utils.shell_exists_win("nu") then
-		config.default_prog = nushell_cmd
-		wezterm.log_info("Using nu shell as default shell")
-	elseif utils.shell_exists_win("pwsh") then
-		config.default_prog = pwsh_cmd
-		wezterm.log_info("Using powershell core as default shell")
-	else
-		config.default_prog = powershell_cmd
-		wezterm.log_info("Using powershell legacy as default shell")
-	end
-elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then -- linux specific
+elseif os_type == utils.OSTypes.Linux then
 	config.enable_wayland = true
-
-	if utils.shell_exists("nu") then
-		config.default_prog = nushell_cmd
-		wezterm.log_info("Using nu shell as default shell")
-	elseif utils.shell_exists("zsh") then
-		config.default_prog = zsh_cmd
-		wezterm.log_info("Using zsh as default shell")
-	else
-		config.default_prog = { "bash", "--login" }
-		wezterm.log_info("Using bash as default shell")
-	end
 end
 
 -- [[ Colour and appearance ]]
 local styling = require("styling")
 styling.apply_to_config(config)
 
--- [[ Keysbinds ]]
+-- [[ Keybinds ]]
 local keybinds = require("keybinds")
 keybinds.apply_to_config(config)
 
