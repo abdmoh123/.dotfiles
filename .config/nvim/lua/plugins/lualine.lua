0c4cb053a30c1df3bfa2f803affbe32d965109eb
abdmoh123 <abdhawisa@gmail.com>
2026-01-10 14:11:03 +0000

    tweak(nvim): lualine to start on VeryLazy event
    Matches Lazyvim distro.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  event = { 'VimEnter' },
+  event = 'VeryLazy',
   opts = {
     options = {
       icons_enabled = true,
