4c693065a25e23fd509a9e909dbefaa3b127c4cf
abdmoh123 <abdhawisa@gmail.com>
2026-01-14 23:21:00 +0000

    refactor(nvim): move indent_line and ufo opts to opts
    Then apply those opts via config function(_, opts).

--- .config/nvim/lua/plugins/indent_line.lua
+++ .config/nvim/lua/plugins/indent_line.lua
@@ -5,14 +5,18 @@ return {
   event = { 'BufReadPost', 'BufNewFile' },
   dependencies = { 'HiPhish/rainbow-delimiters.nvim' },
   main = 'ibl',
-  config = function()
-    require('ibl').setup {
-      indent = { char = '▏' },
-      scope = { highlight = colours.rainbow_highlights },
-    }
-
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {
+    indent = { char = '▏' },
+    scope = { highlight = colours.rainbow_highlights },
+  },
+  ---@param opts ibl.config
+  config = function(_, opts)
     -- makes the scope colour match with the indentation level and rainbow delimiters
     local hooks = require 'ibl.hooks'
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+    require('ibl').setup(opts)
   end,
 }
--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -2,7 +2,26 @@ return {
   'kevinhwang91/nvim-ufo',
   dependencies = { 'kevinhwang91/promise-async' },
   event = { 'VimEnter' }, -- BufReadPre causes errors sometimes
-  config = function()
+  opts = {
+    provider_selector = function(
+      _ --[[ bufnr ]],
+      _ --[[ filetype ]],
+      _ --[[ buftype ]]
+    )
+      return { 'lsp', 'indent' }
+    end,
+    preview = {
+      win_config = {
+        winhighlight = 'Normal:Folded',
+        winblend = 0,
+      },
+    },
+    mappings = {
+      scrollU = '<C-u>',
+      scrollD = '<C-d>',
+    },
+  },
+  config = function(_, opts)
     -- Tell the server the capability of foldingRange,
     -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
     local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -11,33 +30,17 @@ return {
       lineFoldingOnly = true,
     }
 
-    require('ufo').setup {
-      provider_selector = function(
-        _ --[[ bufnr ]],
-        _ --[[ filetype ]],
-        _ --[[ buftype ]]
-      )
-        return { 'lsp', 'indent' }
-      end,
-      preview = {
-        win_config = {
-          winhighlight = 'Normal:Folded',
-          winblend = 0,
-        },
-      },
-      mappings = {
-        scrollU = '<C-u>',
-        scrollD = '<C-d>',
-      },
-    }
+    local ufo = require 'ufo'
+
+    ufo.setup(opts)
 
     -- folding keybinds using ufo
-    vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
+    vim.keymap.set('n', 'zR', ufo.openAllFolds, { desc = 'Open all folds' })
+    vim.keymap.set('n', 'zM', ufo.closeAllFolds, { desc = 'Close all folds' })
 
     -- preview fold
     vim.keymap.set('n', 'zp', function()
-      require('ufo').peekFoldedLinesUnderCursor()
+      ufo.peekFoldedLinesUnderCursor()
     end, { desc = 'Preview fold' })
   end,
 }
