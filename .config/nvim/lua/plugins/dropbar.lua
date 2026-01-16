a229c8ed3e5c9845ef3f4330cd5532c90e93e8cb
abdmoh123 <abdhawisa@gmail.com>
2026-01-16 01:37:15 +0000

    fix(nvim): dropbar not loading itself properly
    Missing require.setup in config function.

--- .config/nvim/lua/plugins/dropbar.lua
+++ .config/nvim/lua/plugins/dropbar.lua
@@ -8,9 +8,10 @@ return {
     build = 'make',
   },
   event = { 'BufReadPost', 'BufNewFile' },
-  config = function()
-    local dropbar_api = require 'dropbar.api'
+  config = function(_, opts)
+    require('dropbar').setup(opts)
 
+    local dropbar_api = require 'dropbar.api'
     vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
     vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
     vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
