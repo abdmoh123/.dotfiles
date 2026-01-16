dbafd71b648357d4b8de3660f7c52aba2dcdba05
abdmoh123 <abdhawisa@gmail.com>
2026-01-16 17:21:50 +0000

    refactor(nvim): move globals to their own module

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,11 +84,7 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require 'globals'
 
 -- improve performance
 vim.loader.enable()
