8fdc79ef16068e0532e7e0566f393976a046699e
abdmoh123 <abdhawisa@gmail.com>
2026-02-13 01:45:35 +0000

    fix(nvim): flutter tools not launching lsp
    Also tweaked the flutter lookup command so it works with mise.

--- .config/nvim/lua/plugins/flutter-tools.lua
+++ .config/nvim/lua/plugins/flutter-tools.lua
@@ -1,9 +1,14 @@
 return {
   'nvim-flutter/flutter-tools.nvim',
-  ft = { 'dart' },
-  dependencies = {
-    'nvim-lua/plenary.nvim',
+  ft = 'dart',
+  dependencies = 'nvim-lua/plenary.nvim',
+  config = function(_, opts)
+    -- for some reason the lsp doesn't work unless this line is added
+    vim.lsp.enable 'dartls'
+
+    require('flutter-tools').setup(opts)
+  end,
+  opts = {
+    flutter_lookup_cmd = 'dirname $(which flutter)',
   },
-  config = true,
-  opts = {},
 }
