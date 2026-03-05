10c1f2295726ed0b96eaaf8fea0ea8fad427b258
abdmoh123 <abdhawisa@gmail.com>
2026-03-05 10:59:36 +0000

    feat(nvim): re-add luasnip snippet engine to blink

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -3,9 +3,34 @@ return {
   'saghen/blink.cmp',
   event = 'InsertEnter',
   dependencies = {
-    'rafamadriz/friendly-snippets',
+    -- Snippet Engine
+    {
+      'L3MON4D3/LuaSnip',
+      version = '2.*',
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        --    See the README about individual language/framework/plugin snippets:
+        --    https://github.com/rafamadriz/friendly-snippets
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+          end,
+        },
+      },
+      opts = {},
+    },
   },
-  version = '*',
+  version = '1.*',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -23,10 +48,6 @@ return {
     },
 
     appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
       -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'normal',
@@ -53,6 +74,8 @@ return {
         },
       },
     },
+
+    snippets = { preset = 'luasnip' },
   },
   opts_extend = { 'sources.default' },
 }
