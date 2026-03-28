9b8c957e633f0f3a4a86af7c8da821611835bc49
abdmoh123 <abdhawisa@gmail.com>
2026-03-28 02:43:57 +0000

    feat(nvim): add overseer plugin
    A task runner plugin

---
+++ .config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,7 @@
+return {
+  'stevearc/overseer.nvim',
+  cmd = { 'OverseerOpen', 'OverseerClose', 'OverseerRun', 'OverseerToggle', 'OverseerShell', 'OverseerTaskAction' },
+  ---@module 'overseer'
+  ---@type overseer.SetupOpts
+  opts = {},
+}
