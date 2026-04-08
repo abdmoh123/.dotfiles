175305da7511ad2a7b8ee8e8e2c6e3b615d4c880
abdmoh123 <abdhawisa@gmail.com>
2026-04-08 18:48:38 +0100

    fix(nvim): add diffview commands as lazy loading conditions
    Allows for running nvim -c Diffview commands.

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -3,5 +3,14 @@
 -- Plugin for easy diff viewing (side-by-side)
 return {
   'sindrets/diffview.nvim',
+  cmd = {
+    'DiffviewOpen',
+    'DiffviewClose',
+    'DiffviewToggleFiles',
+    'DiffviewFocusFiles',
+    'DiffviewFileHistory',
+    'DiffviewLog',
+    'DiffviewRefresh',
+  },
   event = 'BufReadPre',
 }
